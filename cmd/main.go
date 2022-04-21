0f734bd28aaa8a7d6b79a6bae514e520783a5606
Syarif Ubaidillah <ubed56pb@gmail.com>
2022-04-22 03:27:03 +0700

    setel default port di 8080 jika tidak menemukan environment $PORT.

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"belajar-go-echo/internal/framework/repository/implementation"
 	"belajar-go-echo/internal/core/service"
 	"belajar-go-echo/internal/framework/database"
+	"belajar-go-echo/internal/framework/repository/implementation"
 	"belajar-go-echo/internal/framework/routes"
 	"belajar-go-echo/internal/framework/transport/controller"
 	"belajar-go-echo/internal/framework/transport/middleware"
+	"fmt"
 	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	db := database.InitDatabase("mysql")
-	userRepo := implementation.NewUserRepository(db)
-	userService := service.NewUserService(userRepo)
-	userController := controller.NewUserController(userService)
-	
-	e := echo.New()
-	middleware.Logging(e)
-	routes.NewUserRoutes(e, userController, middleware.JWT())
-	e.Start(":" + os.Getenv("PORT"))
+		db := database.InitDatabase("mysql")
+		userRepo := implementation.NewUserRepository(db)
+		userService := service.NewUserService(userRepo)
+		userController := controller.NewUserController(userService)
+
+		e := echo.New()
+		middleware.Logging(e)
+		routes.NewUserRoutes(e, userController, middleware.JWT())
+
+		port, avail := os.LookupEnv("PORT")
+		fmt.Println(port)
+		if !avail {
+				port = "8080"
+		}
+
+		e.Start(":" + port)
 }
